4a658af11d92beac106086e1c6665b952ff29e3e
PANKORN.WANG <77049442+pankornsupercool@users.noreply.github.com>
2023-02-03 16:15:51 +0700

    ssss

---
+++ API/Program.cs
@@ -0,0 +1,23 @@
+using API.Data;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt =>{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+// Learn more about configurin  g Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors();
+builder.Services.AddScoped<ITokenService,TokenService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+
+app.MapControllers();
+
+app.Run();
---
+++ API/Services/TokenService.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Services
+{
+    public class TokenService : ITokenService
+    {
+
+        private readonly SymmetricSecurityKey _key;
+        
+        public TokenService(IConfiguration config){
+            
+        }
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
